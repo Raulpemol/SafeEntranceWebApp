8afa391af76724de57842add8a23a551f7bf878a
Raulpemol <UO263743@uniovi.es>
2021-03-12 13:27:25 +0100

    Added simple client validations

--- src/components/RegisterPlace/RegisterPlace.js
+++ src/components/RegisterPlace/RegisterPlace.js
@@ -11,18 +11,78 @@ class RegisterPlace extends Component {
         super(props);
 
         this.state = {
-            capacityValue: ''
+            name: '',
+            invalidName: false,
+            address: '',
+            invalidAddress: false,
+            capacityValue: '',
+            invalidCapacity: false
         };
 
-        this.handleNonNumericInput = this.handleNonNumericInput.bind(this);
+        this.handleNameInput = this.handleNameInput.bind(this);
+        this.handleAddressInput = this.handleAddressInput.bind(this);
+        this.handleCapacityInput = this.handleCapacityInput.bind(this);
+        this.generateQr = this.generateQr.bind(this);
     }
 
-    handleNonNumericInput(e) {
+    handleNameInput(e){
+        const input = e.target.value;
+        this.setState({
+            name: input,
+            invalidName: false
+        });
+    }
+
+    handleAddressInput(e){
+        const input = e.target.value;
+        this.setState({
+            address: input,
+            invalidAddress: false
+        });
+    }
+
+    handleCapacityInput(e) {
         const input = e.target.value;
         const numbers = input.replace(/[^0-9]/, '');
         this.setState({
-            capacityValue: numbers
+            capacityValue: numbers,
+            invalidCapacity: false
         });
+    }
+
+    validateFields(){
+        let isValid = true;
+
+        if(this.state.name == null || this.state.name.length < 1){
+            this.setState({
+                name: '',
+                invalidName: true
+            });
+            isValid = false;
+        }
+        if(this.state.address == null || this.state.address.length < 1){
+            this.setState({
+                address: '',
+                invalidAddress: true
+            });
+            isValid = false;
+        }
+        if(this.state.capacityValue == null || this.state.capacityValue == ''){
+            this.setState({
+                capacityValue: '',
+                invalidCapacity: true
+            });
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    generateQr(e){
+        e.preventDefault();
+        if(this.validateFields()){
+            
+        }
     }
 
     render(){
@@ -30,18 +90,26 @@ class RegisterPlace extends Component {
             <div className="Form" style={{backgroundImage:"url('/img/green_background.jpg')"}}>
                 <Card className="Card" variant="outlined" style={{backgroundColor: "#baf2e9"}}>
                     <CardContent>
-                        <form noValidate autoComplete="off">
-                            <TextField className="FormInput" variant="outlined" id="nameField" label="Nombre del local"/>
-                            <p></p>
-                            <TextField className="FormInput" variant="outlined" id="addressField" label="Dirección"/>
-                            <p></p>
-                            <TextField className="FormInput" variant="outlined" id="capacityField" label="Aforo" type="number"
-                                defaultValue={this.state.capacityValue}
-                                onChange={this.handleNonNumericInput}
-                                value={this.state.capacityValue}
+                        <form onSubmit={this.generateQr} autoComplete="off">
+                            <TextField className="FormInput" variant="outlined" id="nameField" label="Nombre del local" 
+                                onChange={this.handleNameInput}
+                                value={this.state.name}
+                                error={this.state.invalidName}
                             />
                             <p></p>
-                            <Button variant="contained" color="primary">
+                            <TextField className="FormInput" variant="outlined" id="addressField" label="Dirección" 
+                                onChange={this.handleAddressInput} 
+                                value={this.state.address}
+                                error={this.state.invalidAddress}
+                            />
+                            <p></p>
+                            <TextField className="FormInput" variant="outlined" id="capacityField" label="Aforo" type="number"
+                                onChange={this.handleCapacityInput}
+                                value={this.state.capacityValue}
+                                error={this.state.invalidCapacity}
+                            />
+                            <p></p>
+                            <Button variant="contained" color="primary" type="submit">
                                 Generar QR
                             </Button>
                         </form>
