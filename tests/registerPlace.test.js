04637b9842dfe703c4e8fc37fd8ec642b824c7ec
Raulpemol <UO263743@uniovi.es>
2021-03-14 12:38:30 +0100

    Simple form tests finished

--- tests/registerPlace.test.js
+++ tests/registerPlace.test.js
@@ -1,9 +1,8 @@
-
 describe('The SafeEntrance web application', () => {
     beforeAll(async () => {
       await page.goto('http://localhost:3000/');
     });
-  
+
     it('should be titled "SafeEntrance"', async () => {
       await expect(page.title()).resolves.toMatch('SafeEntrance');
     });
@@ -20,3 +19,63 @@ describe('The SafeEntrance web application', () => {
         await expect(page).toMatchElement('button[id="submitButton"]');
     });
   });
+
+describe('The form fields', () => {
+    it('should be marked as invalid when empty"', async () => {
+        await expect(page).toClick('button', { id: 'submitButton' })
+        await expect(page).toMatchElement('input[id="nameField"][aria-invalid="true"]');
+        await expect(page).toMatchElement('input[id="addressField"][aria-invalid="true"]');
+        await expect(page).toMatchElement('input[id="capacityField"][aria-invalid="true"]');
+    });
+});
+
+describe('The input field for the name', () => {
+    beforeAll(async () => {
+      await page.goto('http://localhost:3000/');
+    });
+
+    it('should not be marked as invalid when filled"', async () => {
+      await expect(page).toFill(
+        'input[id="nameField"]',
+        "Local de Prueba"
+      );
+      await expect(page).toClick('button', { id: 'submitButton' })
+      await expect(page).toMatchElement('input[id="nameField"][aria-invalid="false"]');
+      await expect(page).toMatchElement('input[id="addressField"][aria-invalid="true"]');
+      await expect(page).toMatchElement('input[id="capacityField"][aria-invalid="true"]');
+    });
+});
+
+describe('The input field for the address', () => {
+    beforeAll(async () => {
+      await page.goto('http://localhost:3000/');
+    });
+
+    it('should not be marked as invalid when filled"', async () => {
+      await expect(page).toFill(
+        'input[id="addressField"]',
+        "c/Dirección de prueba, Nº8"
+      );
+      await expect(page).toClick('button', { id: 'submitButton' })
+      await expect(page).toMatchElement('input[id="nameField"][aria-invalid="true"]');
+      await expect(page).toMatchElement('input[id="addressField"][aria-invalid="false"]');
+      await expect(page).toMatchElement('input[id="capacityField"][aria-invalid="true"]');
+    });
+});
+
+describe('The input field for the capacity', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:3000/');
+  });
+
+  it('should not be marked as invalid when filled"', async () => {
+    await expect(page).toFill(
+      'input[id="capacityField"]',
+      "1"
+    );
+    await expect(page).toClick('button', { id: 'submitButton' })
+    await expect(page).toMatchElement('input[id="nameField"][aria-invalid="true"]');
+    await expect(page).toMatchElement('input[id="addressField"][aria-invalid="true"]');
+    await expect(page).toMatchElement('input[id="capacityField"][aria-invalid="false"]');
+  });
+});
