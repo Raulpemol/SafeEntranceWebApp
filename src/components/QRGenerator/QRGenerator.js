6c95fa04a9c1490493d87b1a954558059da6de0d
Raulpemol <UO263743@uniovi.es>
2021-03-14 20:50:11 +0100

    Added qr view and buttons

--- src/components/QRGenerator/QRGenerator.js
+++ src/components/QRGenerator/QRGenerator.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './QRGenerator.css';
-import { Link } from "react-router-dom";
 import QRcode from 'qrcode.react';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 class QRGenerator extends Component {
 
@@ -11,7 +12,20 @@ class QRGenerator extends Component {
         return(
             <div className="Main" style={{backgroundImage:"url('/img/green_background.jpg')"}}>
                 <Card className="Card" variant="outlined" style={{backgroundColor: "#baf2e9"}}>
-
+                    <QRcode 
+                        id="qr"
+                        value={"prueba"} 
+                        size={300}
+                    />
+                    <p></p>
+                    <Button variant="contained" color="primary" id="submitButton" style={{margin: "10px"}}
+                        startIcon={<ArrowBackIcon />}>
+                        Volver
+                    </Button>
+                    <Button variant="contained" color="primary" id="submitButton" style={{margin: "10px"}}
+                        startIcon={<GetAppIcon />}>
+                        Descargar
+                    </Button>
                 </Card>
             </div>
         );
