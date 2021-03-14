1b6a87c68ea69744938bad2c31eb0dc0cc4dd78e
Raulpemol <UO263743@uniovi.es>
2021-03-14 20:52:50 +0100

    Centered card

--- src/components/QRGenerator/QRGenerator.js
+++ src/components/QRGenerator/QRGenerator.js
@@ -5,28 +5,31 @@ import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import Grid from '@material-ui/core/Grid';
 
 class QRGenerator extends Component {
 
     render(){
         return(
             <div className="Main" style={{backgroundImage:"url('/img/green_background.jpg')"}}>
-                <Card className="Card" variant="outlined" style={{backgroundColor: "#baf2e9"}}>
-                    <QRcode 
-                        id="qr"
-                        value={"prueba"} 
-                        size={300}
-                    />
-                    <p></p>
-                    <Button variant="contained" color="primary" id="submitButton" style={{margin: "10px"}}
-                        startIcon={<ArrowBackIcon />}>
-                        Volver
-                    </Button>
-                    <Button variant="contained" color="primary" id="submitButton" style={{margin: "10px"}}
-                        startIcon={<GetAppIcon />}>
-                        Descargar
-                    </Button>
-                </Card>
+                <Grid container spacing={0} direction="column" alignItems="center" justify="center">
+                    <Card className="Card" variant="outlined" style={{backgroundColor: "#baf2e9"}}>
+                        <QRcode 
+                            id="qr"
+                            value={"prueba"} 
+                            size={300}
+                        />
+                        <p></p>
+                        <Button variant="contained" color="primary" id="submitButton" style={{margin: "10px"}}
+                            startIcon={<ArrowBackIcon />}>
+                            Volver
+                        </Button>
+                        <Button variant="contained" color="primary" id="submitButton" style={{margin: "10px"}}
+                            startIcon={<GetAppIcon />}>
+                            Descargar
+                        </Button>
+                    </Card>
+                </Grid>
             </div>
         );
     }
