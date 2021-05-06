fa1d4fa5e89baaede485f856e6c16e530d9d1221
Raulpemol <UO263743@uniovi.es>
2021-05-06 20:35:32 +0200

    Refactored snackbars

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -2,14 +2,9 @@ import React, { Component } from 'react';
 import './AdminPanel.css';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { getOneParameterResponse, getPost } from '../../services/HttpManager';
+import { getOneParameterResponse } from '../../services/HttpManager';
 import Grid from '@material-ui/core/Grid';
 import { withRouter } from 'react-router-dom';
-import { Base64 } from "js-base64";
-import Snackbar from '@material-ui/core/Snackbar';
-import Alert from '@material-ui/lab/Alert';
 import VariableComponent from '../VariableComponent/VariableComponent';
 
 class RegisterPlace extends Component {
@@ -24,19 +19,11 @@ class RegisterPlace extends Component {
             getVariablesUrl: 'https://registrolocales-api.azurewebsites.net/env/getVariables'
         };
 
-        this.handleAlertClose = this.handleAlertClose.bind(this);
         this.getVariables = this.getVariables.bind(this);
     }
 
     componentDidMount(){
         this.getVariables();
-    }
-
-    handleAlertClose(){
-        this.setState({
-            error: false,
-            message: false
-        });
     }
 
     async getVariables(){
@@ -54,40 +41,27 @@ class RegisterPlace extends Component {
             return null;
         }
         else{
-        const variables = this.state.variables;
-        const token = this.state.token;
-        return(
-            <div className="Form" style={{backgroundImage:"url('/img/green_background.jpg')"}}>
-            <Grid container spacing={0} direction="column" alignItems="center" justify="center">
-                <Card className="Panel" variant="outlined" style={{backgroundColor: "#baf2e9"}}>
-                    <h2 id="subtitlePanel">Variables de entorno</h2>
-                    <CardContent>
-                        <Grid container spacing={2} style={{justifyContent: "center", alignItems: "center"}}>
-                            {
-                                variables.map(function(item){
-                                    return(<VariableComponent key={item._id} name={item.name} value={item.value} description={item.description}
-                                        token={token}/>)
-                                })
-                            }
-                        </Grid>
-                        <Snackbar id="errorAlert" open={this.state.error} autoHideDuration={5000} onClose={this.handleAlertClose}
-                            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
-                            <Alert severity="error" onClose={this.handleAlertClose}>
-                                Valor incorrecto
-                            </Alert>
-                        </Snackbar>
-                        <Snackbar id="messageAlert" open={this.state.message} autoHideDuration={5000} onClose={this.handleAlertClose}
-                            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
-                            <Alert severity="success" onClose={this.handleAlertClose}>
-                                Se ha actualizado el valor correctamente
-                            </Alert>
-                        </Snackbar>
-                    </CardContent>
-                </Card>
-            </Grid>
-        </div>
-            
-        );
+            const variables = this.state.variables;
+            const token = this.state.token;
+            return(
+                <div className="Form" style={{backgroundImage:"url('/img/green_background.jpg')"}}>
+                    <Grid container spacing={0} direction="column" alignItems="center" justify="center">
+                        <Card className="Panel" variant="outlined" style={{backgroundColor: "#baf2e9"}}>
+                            <h2 id="subtitlePanel">Variables de entorno</h2>
+                            <CardContent>
+                                <Grid container spacing={2} style={{justifyContent: "center", alignItems: "center"}}>
+                                    {
+                                        variables.map(function(item){
+                                            return(<VariableComponent key={item._id} name={item.name} value={item.value} description={item.description}
+                                                token={token}/>)
+                                        })
+                                    }
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </div>
+            );
         }
     }
 }
--- src/components/VariableComponent/VariableComponent.js
+++ src/components/VariableComponent/VariableComponent.js
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
-import Card from '@material-ui/core/Card';
-import QRcode from 'qrcode.react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 import { getPost } from '../../services/HttpManager';
-
-
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
 
 function VariableComponent(props){
     const { name, value, description, token } = props;
-    const baseApiUrl = 'https://registrolocales-api.azurewebsites.net/env/set';
+    const baseApiUrl = 'https://registrolocales-api.azurewebsites.net/env/setVariable';
     const [state, setState] = useState({
         value: value,
-        invalid: false
+        invalid: false,
+        alert: false,
+        message: false
     });
 
     function handleInput(e) {
@@ -23,16 +22,17 @@ function VariableComponent(props){
         setState({
             value: numbers,
             invalid: false,
+            alert: false,
             message: false
         });
     }
 
     async function setVariable(e){
         e.preventDefault();
-        const url = baseApiUrl + name;
-        const response = await getPost(url, 
+        const response = await getPost(baseApiUrl, 
         {
             "token": token,
+            "name": name,
             "value": state.value
         });
             
@@ -40,21 +40,32 @@ function VariableComponent(props){
             setState({
                 value: state.value,
                 invalid: true,
+                alert: true,
                 message: false
             });
         }
         else{
             setState({
-                message: true,
                 value: state.value,
-                invalid: false
+                invalid: false,
+                alert: false,
+                message: true
             });
         }
     }
 
+    function handleAlertClose(){
+        setState({
+            value: state.value,
+            invalid: false,
+            alert: false,
+            message: false
+        });
+    }
+
     return(
         <Grid item xs={12} sm={4}>
-            <TextField className="InputVar" variant="filled" type="number" label={description}
+            <TextField className="inputVar" variant="outlined" type="number" label={description}
                 onChange={handleInput}
                 value={state.value}
                 error={state.invalid}
@@ -63,6 +74,18 @@ function VariableComponent(props){
                 onClick={setVariable}>
                 Guardar
             </Button>
+            <Snackbar id="errorAlert" open={state.alert} autoHideDuration={5000} onClose={handleAlertClose}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+                <Alert severity="error" onClose={handleAlertClose}>
+                    Valor no permitido
+                </Alert>
+            </Snackbar>
+            <Snackbar id="messageAlert" open={state.message} autoHideDuration={5000} onClose={handleAlertClose}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+                <Alert severity="success" onClose={handleAlertClose}>
+                    Se ha actualizado el valor correctamente
+                </Alert>
+            </Snackbar>
         </Grid>
     );
 }
